66cc685c490a89737c4bb39b2e14aaa7357805c2
Daniel Martínez Suárez <danielmartinezisc98@outlook.com>
2022-03-29 18:06:04 -0600

    Se agregó el Breadcrumb al inicio del contenido

--- resources/views/assets/components/footer.blade.php
+++ resources/views/assets/components/footer.blade.php
@@ -1,11 +1,10 @@
 <footer class="footer">
     <div class="row g-0 justify-content-between fs--1 mt-4 mb-3">
-        <div class="col-12 col-sm-auto text-center">
-            <p class="mb-0 text-600">Thank you for creating with Falcon <span class="d-none d-sm-inline-block">|
-                </span><br class="d-sm-none"> 2022 © <a href="https://themewagon.com">Themewagon</a></p>
-        </div>
-        <div class="col-12 col-sm-auto text-center">
-            <p class="mb-0 text-600">v3.5.0</p>
+        <div class="col-sm-12 text-center">
+            <p class="mb-0 text-600">
+                <span class="d-none d-sm-inline-block">Sistema Integral de Información | 2022 ©</span>
+                <a href="https://upq.mx">UPQ</a>
+            </p>
         </div>
     </div>
 </footer>
--- resources/views/panel-principal.blade.php
+++ resources/views/panel-principal.blade.php
@@ -2,48 +2,56 @@
 
 @php
 $modulos_nuevo_ingreso = [
-    'formato_solicitud_2' => 'Continuar Proceso - Formato de Solicitud 2',
-    'encuesta_generica' => 'Encuesta generica (Encuesta de orientacion etc...)',
-    'carga_documentos' => 'Carga de Documentos',
-    'panel_servicios_escolares' => 'Panel Servicios Escolares',
-    'cursos' => 'Cursos'
+'formato_solicitud_2' => 'Continuar Proceso - Formato de Solicitud 2',
+'encuesta_generica' => 'Encuesta generica (Encuesta de orientacion etc...)',
+'carga_documentos' => 'Carga de Documentos',
+'panel_servicios_escolares' => 'Panel Servicios Escolares',
+'cursos' => 'Cursos'
 ];
 $modulos_alumno_prospecto = [
-    'formato_solicitud_1' => 'Formato solicitud No. 1',
-    'inicio_alumno_prospecto' => 'Inicio Alumno/Prospecto',
-    'panel_prospecto' => 'Panel Prospecto',
-    'acceso_prospecto' => 'Acceso Prospecto',    
+'formato_solicitud_1' => 'Formato solicitud No. 1',
+'inicio_alumno_prospecto' => 'Inicio Alumno/Prospecto',
+'panel_prospecto' => 'Panel Prospecto',
+'acceso_prospecto' => 'Acceso Prospecto',
 ];
 $modulos_servicios_escolares = [
-    'generar_horarios' => 'Generar Horarios',
-    'validacion_documentos_fotos' => 'Validación documentos y fotos',
-    'agenda' => 'Agenda',
+'generar_horarios' => 'Generar Horarios',
+'validacion_documentos_fotos' => 'Validación documentos y fotos',
+'agenda' => 'Agenda',
+'proyecciones' => 'Proyecciones',
 ];
 $modulos_secretaria_academica = [
-    'validar_horarios_grupos' => 'Validar Horarios y Grupos',
-    'validar_agenda' => 'Validar Agenda',
-    'validar_proyecciones' => 'Validar Proyecciones',
-    'validar_transporte' => 'Validar transporte',
+'validar_horarios_grupos' => 'Validar Horarios y Grupos',
+'validar_agenda' => 'Validar Agenda',
+'validar_proyecciones' => 'Validar Proyecciones',
+'validar_transporte' => 'Validar transporte',
 ];
 $modulos_direccion_carrera = [
-    'consulta_grupos' => 'Consultar grupos - Curso introducción',
-    'asignar_profesor' =>'Asignar profesores - Curso Introducción',
-    'bachillerato_avanzado_consultar_grupos' =>'Consultar grupos - Bachillerato avanzado',
-    'bachillerato_avanzado_asignar_profesores' =>'Asignar profesores - Bachillerato avanzado',
+'consulta_grupos' => 'Consultar grupos - Curso introducción',
+'asignar_profesor' =>'Asignar profesores - Curso Introducción',
+'bachillerato_avanzado_consultar_grupos' =>'Consultar grupos - Bachillerato avanzado',
+'bachillerato_avanzado_asignar_profesores' =>'Asignar profesores - Bachillerato avanzado',
 ];
 $modulos_orientacion_educativa = [
-    'captura_psicometrix' => 'Caputra de Psicometrix',
-    'asignar_materias_grupos' => 'Asignar Materias a Grupos',
-    'consultar_programacion_grupos' => 'Consultar Programación de Grupos',
+'captura_psicometrix' => 'Caputra de Psicometrix',
+'asignar_materias_grupos' => 'Asignar Materias a Grupos',
+'consultar_programacion_grupos' => 'Consultar Programación de Grupos',
 ];
 $modulos_profesor_curso = [
-    'profesor_curso_consultar_grupos' => 'Consultar grupos',
-    'profesor_curso_ci_capturar_calificaciones' => 'Capturar calificaciones | Cursos de Inducción',
-    'profesor_curso_ba_capturar_calificaciones' => 'Capturar calificaciones | Bachillerato Avanzado',
+'profesor_curso_consultar_grupos' => 'Consultar grupos',
+'profesor_curso_ci_capturar_calificaciones' => 'Capturar calificaciones | Cursos de Inducción',
+'profesor_curso_ba_capturar_calificaciones' => 'Capturar calificaciones | Bachillerato Avanzado',
 ];
 @endphp
 
 @section('content')
+<nav style="--falcon-breadcrumb-divider: '»';" aria-label="breadcrumb" class="mb-2">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="">Menú</a></li>
+        <li class="breadcrumb-item"><a href="#">Servicios escolares</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Proyecciones</li>
+    </ol>
+</nav>
 <div class="row">
     <div class="col-sm-6 col-lg-4 mb-4">
         <div class="card overflow-hidden" style="width: 20rem;">
@@ -59,14 +67,14 @@ $modulos_profesor_curso = [
                     <div class="border p-card rounded">
                         <ul class="list-group list-group-flush">
                             @foreach ($modulos_nuevo_ingreso as $key => $value)
-                                <li class="list-group-item">
-                                    <a class="nav-link p-0" href="app/calendar.html" role="button" aria-expanded="false">
-                                        <div class="d-flex align-items-center">
-                                            <span class="far fa-address-book text-dark fs-1 me-1"></span>
-                                            <span class="nav-link-text ps-1 text-dark">{{ $value }}</span>
-                                        </div>
-                                    </a>
-                                </li>
+                            <li class="list-group-item">
+                                <a class="nav-link p-0" href="app/calendar.html" role="button" aria-expanded="false">
+                                    <div class="d-flex align-items-center">
+                                        <span class="far fa-address-book text-dark fs-1 me-1"></span>
+                                        <span class="nav-link-text ps-1 text-dark">{{ $value }}</span>
+                                    </div>
+                                </a>
+                            </li>
                             @endforeach
                         </ul>
                     </div>
@@ -88,14 +96,14 @@ $modulos_profesor_curso = [
                     <div class="border p-card rounded">
                         <ul class="list-group list-group-flush">
                             @foreach ($modulos_alumno_prospecto as $key => $value)
-                                <li class="list-group-item">
-                                    <a class="nav-link p-0" href="app/calendar.html" role="button" aria-expanded="false">
-                                        <div class="d-flex align-items-center">
-                                            <span class="far fa-address-book text-dark fs-1 me-1"></span>
-                                            <span class="nav-link-text ps-1 text-dark">{{ $value }}</span>
-                                        </div>
-                                    </a>
-                                </li>
+                            <li class="list-group-item">
+                                <a class="nav-link p-0" href="app/calendar.html" role="button" aria-expanded="false">
+                                    <div class="d-flex align-items-center">
+                                        <span class="far fa-address-book text-dark fs-1 me-1"></span>
+                                        <span class="nav-link-text ps-1 text-dark">{{ $value }}</span>
+                                    </div>
+                                </a>
+                            </li>
                             @endforeach
                         </ul>
                     </div>
@@ -117,14 +125,14 @@ $modulos_profesor_curso = [
                     <div class="border p-card rounded">
                         <ul class="list-group list-group-flush">
                             @foreach ($modulos_servicios_escolares as $key => $value)
-                                <li class="list-group-item">
-                                    <a class="nav-link p-0" href="app/calendar.html" role="button" aria-expanded="false">
-                                        <div class="d-flex align-items-center">
-                                            <span class="far fa-address-book text-dark fs-1 me-1"></span>
-                                            <span class="nav-link-text ps-1 text-dark">{{ $value }}</span>
-                                        </div>
-                                    </a>
-                                </li>
+                            <li class="list-group-item">
+                                <a class="nav-link p-0" href="app/calendar.html" role="button" aria-expanded="false">
+                                    <div class="d-flex align-items-center">
+                                        <span class="far fa-address-book text-dark fs-1 me-1"></span>
+                                        <span class="nav-link-text ps-1 text-dark">{{ $value }}</span>
+                                    </div>
+                                </a>
+                            </li>
                             @endforeach
                         </ul>
                     </div>
@@ -146,14 +154,14 @@ $modulos_profesor_curso = [
                     <div class="border p-card rounded">
                         <ul class="list-group list-group-flush">
                             @foreach ($modulos_secretaria_academica as $key => $value)
-                                <li class="list-group-item">
-                                    <a class="nav-link p-0" href="app/calendar.html" role="button" aria-expanded="false">
-                                        <div class="d-flex align-items-center">
-                                            <span class="far fa-address-book text-dark fs-1 me-1"></span>
-                                            <span class="nav-link-text ps-1 text-dark">{{ $value }}</span>
-                                        </div>
-                                    </a>
-                                </li>
+                            <li class="list-group-item">
+                                <a class="nav-link p-0" href="app/calendar.html" role="button" aria-expanded="false">
+                                    <div class="d-flex align-items-center">
+                                        <span class="far fa-address-book text-dark fs-1 me-1"></span>
+                                        <span class="nav-link-text ps-1 text-dark">{{ $value }}</span>
+                                    </div>
+                                </a>
+                            </li>
                             @endforeach
                         </ul>
                     </div>
@@ -175,14 +183,14 @@ $modulos_profesor_curso = [
                     <div class="border p-card rounded">
                         <ul class="list-group list-group-flush">
                             @foreach ($modulos_direccion_carrera as $key => $value)
-                                <li class="list-group-item">
-                                    <a class="nav-link p-0" href="app/calendar.html" role="button" aria-expanded="false">
-                                        <div class="d-flex align-items-center">
-                                            <span class="far fa-address-book text-dark fs-1 me-1"></span>
-                                            <span class="nav-link-text ps-1 text-dark">{{ $value }}</span>
-                                        </div>
-                                    </a>
-                                </li>
+                            <li class="list-group-item">
+                                <a class="nav-link p-0" href="app/calendar.html" role="button" aria-expanded="false">
+                                    <div class="d-flex align-items-center">
+                                        <span class="far fa-address-book text-dark fs-1 me-1"></span>
+                                        <span class="nav-link-text ps-1 text-dark">{{ $value }}</span>
+                                    </div>
+                                </a>
+                            </li>
                             @endforeach
                         </ul>
                     </div>
@@ -197,21 +205,22 @@ $modulos_profesor_curso = [
             <div class="card-body">
                 <h5 class="card-title">Orientación educativa</h5>
                 <p>
-                    <a class="btn btn-falcon-default mt-2" data-bs-toggle="collapse" href="#collapseOrientacionEducativa"
-                        role="button" aria-expanded="false" aria-controls="collapseExample">Módulos</a>
+                    <a class="btn btn-falcon-default mt-2" data-bs-toggle="collapse"
+                        href="#collapseOrientacionEducativa" role="button" aria-expanded="false"
+                        aria-controls="collapseExample">Módulos</a>
                 </p>
                 <div class="collapse" id="collapseOrientacionEducativa">
                     <div class="border p-card rounded">
                         <ul class="list-group list-group-flush">
                             @foreach ($modulos_direccion_carrera as $key => $value)
-                                <li class="list-group-item">
-                                    <a class="nav-link p-0" href="app/calendar.html" role="button" aria-expanded="false">
-                                        <div class="d-flex align-items-center">
-                                            <span class="far fa-address-book text-dark fs-1 me-1"></span>
-                                            <span class="nav-link-text ps-1 text-dark">{{ $value }}</span>
-                                        </div>
-                                    </a>
-                                </li>
+                            <li class="list-group-item">
+                                <a class="nav-link p-0" href="app/calendar.html" role="button" aria-expanded="false">
+                                    <div class="d-flex align-items-center">
+                                        <span class="far fa-address-book text-dark fs-1 me-1"></span>
+                                        <span class="nav-link-text ps-1 text-dark">{{ $value }}</span>
+                                    </div>
+                                </a>
+                            </li>
                             @endforeach
                         </ul>
                     </div>
@@ -233,14 +242,14 @@ $modulos_profesor_curso = [
                     <div class="border p-card rounded">
                         <ul class="list-group list-group-flush">
                             @foreach ($modulos_profesor_curso as $key => $value)
-                                <li class="list-group-item">
-                                    <a class="nav-link p-0" href="app/calendar.html" role="button" aria-expanded="false">
-                                        <div class="d-flex align-items-center">
-                                            <span class="far fa-address-book text-dark fs-1 me-1"></span>
-                                            <span class="nav-link-text ps-1 text-dark">{{ $value }}</span>
-                                        </div>
-                                    </a>
-                                </li>
+                            <li class="list-group-item">
+                                <a class="nav-link p-0" href="app/calendar.html" role="button" aria-expanded="false">
+                                    <div class="d-flex align-items-center">
+                                        <span class="far fa-address-book text-dark fs-1 me-1"></span>
+                                        <span class="nav-link-text ps-1 text-dark">{{ $value }}</span>
+                                    </div>
+                                </a>
+                            </li>
                             @endforeach
                         </ul>
                     </div>
@@ -248,6 +257,6 @@ $modulos_profesor_curso = [
             </div>
         </div>
     </div>
-    
+
 </div>
 @endsection
