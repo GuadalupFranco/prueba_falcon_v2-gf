9d8539e46819de99f28c5e73b66c736d22734528
Gerson Pérez <gpc9815@gmail.com>
2022-03-31 14:00:48 -0600

    Estructura de carpetas (models, controllers, etc) -Modulo Administración de usuarios -Modulo Administración de periodos

---
+++ app/Http/Controllers/modelos/super-usuario/administracion-periodos/PeriodController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\modelos\super-usuario\administracion-periodos;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PeriodController extends Controller
+{
+    function index(){
+
+    }
+    
+}
---
+++ database/factories/Modulos/superUsuario/administracionPeriodos/PeriodFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\modulos\super-usuario\administracion-periodos;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\modulos\super-usuario\administracion-periodos\Period>
+ */
+class PeriodFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
