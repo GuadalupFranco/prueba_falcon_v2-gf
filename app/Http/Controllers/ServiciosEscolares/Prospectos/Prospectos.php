3cbfa142db4e0b5ca2476bea7a088dc0d0fedc83
Daniel Martínez Suárez <danielmartinezisc98@outlook.com>
2022-04-03 22:06:25 -0500

    Interfaces de Servicios Escolares

---
+++ app/Http/Controllers/ServiciosEscolares/Prospectos/Prospectos.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\ServiciosEscolares\Prospectos;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Prospectos extends Controller
+{
+    public function index(){
+        return view('Modulos.ServiciosEscolares.Prospectos.index');
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\ServiciosEscolares\{ControlFichas\ControlFichasController, Prospectos\Prospectos};
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/', function () {
     return view('panel-principal');
@@ -20,3 +10,6 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::resource('ControlFichas', ControlFichasController::class)->middleware('auth');
+Route::resource('Prospectos', Prospectos::class)->middleware('auth');
