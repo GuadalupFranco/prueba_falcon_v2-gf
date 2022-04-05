956dbc0c406813c078311a6123460d1ee1b03229
Daniel Martínez Suárez <danielmartinezisc98@outlook.com>
2022-04-05 18:43:18 -0500

    Actualización

--- resources/views/Modulos/ServiciosEscolares/Agenda/index.blade.php
+++ resources/views/Modulos/ServiciosEscolares/Agenda/index.blade.php
@@ -1,7 +1,7 @@
 @extends('welcome')
 
 @section('extra-styles')
-<link href="/vendors/fullcalendar/main.min.css" rel="stylesheet"/>
+<link href="/vendors/fullcalendar/main.min.css" rel="stylesheet" />
 @endsection
 
 @section('content')
@@ -1112,39 +1112,6 @@
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
-<!-- Modal -->
-<div class="modal fade" id="staticBackdrop" data-bs-keyboard="false" data-bs-backdrop="static" tabindex="-1"
-    aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg mt-6" role="document">
-        <div class="modal-content border-0">
-            <div class="position-absolute top-0 end-0 mt-3 me-3 z-index-1">
-                <button class="btn-close btn btn-sm btn-circle d-flex flex-center transition-base"
-                    data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body p-0">
-                <div class="bg-light rounded-top-lg py-3 ps-4 pe-6">
-                    @yield('modal-header')
-                    <h4 class="mb-1" id="staticBackdropLabel">Subir calificaciones</h4>
-                </div>
-                @yield('modal-body')
-                <form action="" method="POST" enctype="multipart/form-data">
-                    <div class="p-4 pb-0">
-                        <div class="mb-3">
-                            <label class="form-label" for="customFile">Subir archivo .CSV</label>
-                            <input class="form-control" id="customFile" type="file" />
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        @yield('modal-footer')
-                        <button class="btn btn-success" type="submit">Guardar</button>
-                        <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Cerrar</button>
-                    </div>
-                </form>
             </div>
         </div>
     </div>
