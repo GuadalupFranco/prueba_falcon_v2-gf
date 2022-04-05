30278c4e3d654ed42a962c844edaf8fe9e801ad2
Gerson Pérez <gpc9815@gmail.com>
2022-04-05 14:00:05 -0500

    Cambios: -Generación de modales para boton de new -Asset de formulario-periodo.

--- resources/views/modulos/super-usuario/administracion-periodos/period/vista-lista-general-periodos.blade.php
+++ resources/views/modulos/super-usuario/administracion-periodos/period/vista-lista-general-periodos.blade.php
@@ -28,100 +28,22 @@
               <span class="d-none d-sm-inline-block ms-1">New</span>
             </button>
             <div class="modal fade" id="error-modal" tabindex="-1" role="dialog" aria-hidden="true">
-              <div class="modal-dialog modal-dialog-centered" role="document" style="max-width: 1000px">
+              <div class="modal-dialog modal-dialog-centered" role="document" style="max-width: 800px">
                 <div class="modal-content position-relative">
                   <div class="position-absolute top-0 end-0 mt-2 me-2 z-index-1">
                     <button class="btn-close btn btn-sm btn-circle d-flex flex-center transition-base" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
-                  <form class="row g-3 needs-validation" novalidate="">
-                    <div class="modal-body p-0">
-
-                    
-                      <div class="card">
-                          <div class="card-header">
-                            <div class="row flex-between-end">
-                              <div class="col-auto align-self-center">
-                                  <h6 class="mb-0">Información general</h6>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="card-body border-top">
-                              <div class="row">
-                                  <div class="col">
-                                      <div class="col-md-6 position-relative">
-                                          <label class="form-label" for="validationTooltip01">Descripción</label>
-                                          <textarea class="form-control" id="validationTooltip01" value="" required=""></textarea>
-                                          <div class="valid-tooltip">Por favor ingresa una descripción en el campo.</div>
-                                      </div>
-                                  </div>
-                                  <div class="col">
-                                      <div class="row">
-                                          <div class="col-md-5 position-relative">
-                                              <label class="form-label" for="validationTooltip04">Periodo superior</label>
-                                              <select class="form-select" id="validationTooltip04" required="">
-                                                  <option selected="" disabled="" value="">seleccionar...</option>
-                                                  <option>1</option>
-                                                  <option>2</option>
-                                                  <option>3</option>
-                                              </select>
-                                              <div class="invalid-tooltip">Por favor selecciona un elemento de la lista válido.</div>
-                                          </div>
-                                      </div>
-                                      <div class="row">
-                                          <div class="col-md-5 position-relative">
-                                              <label class="form-label" for="validationTooltip04">Tarea relacionada</label>
-                                              <select class="form-select" id="validationTooltip04" required="">
-                                                  <option selected="" disabled="" value="">seleccionar...</option>
-                                                  <option>1</option>
-                                                  <option>2</option>
-                                                  <option>3</option>
-                                              </select>
-                                              <div class="invalid-tooltip">Por favor selecciona un elemento de la lista válido.</div>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div class="card border-top my-0">
-                          <div class="card-header">
-                            <div class="row flex-between-end">
-                              <div class="col-auto align-self-center">
-                                  <h6 class="mb-0">Especificaciones</h6>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="card-body border-top">
-                              <div class="row">
-                                  <div class="col-md-3 position-relative">
-                                      <label class="form-label" for="validationTooltip04">Estatus</label>
-                                      <select class="form-select" id="validationTooltip04" required="">
-                                          <option selected="" disabled="" value="">seleccionar...</option>
-                                          <option>Activo</option>
-                                          <option>Inactivo</option>
-                                      </select>
-                                      <div class="invalid-tooltip">Por favor selecciona un elemento de la lista válido.</div>
-                                  </div>
-                                  <div class="col-md-2 position-relative">
-                                      <label class="form-label" for="datepicker_ini">Fecha de inicio</label>
-                                      <input class="form-control datetimepicker" id="datepicker_ini" type="text" placeholder="d/m/y" data-options='{"disableMobile":true}'/>
-                                      <div class="valid-tooltip">Selecciona una fecha inicial válida</div>
-                                  </div>
-                                  <div class="col-md-2 position-relative">
-                                      <label class="form-label" for="datepicker_fin">Fecha de fin</label>
-                                      <input class="form-control datetimepicker" id="datepicker_fin" type="text" placeholder="d/m/y" data-options='{"disableMobile":true}'/>
-                                      <div class="valid-tooltip">Selecciona una fecha final válida</div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                    
-
+                  <div class="modal-body p-0">
+                    <div class="rounded-top-lg py-3 ps-4 pe-6 bg-light">
+                      <h4 class="mb-1" id="modalExampleDemoLabel"> Registrar nuevo periodo </h4>
                     </div>
-                    <div class="modal-footer">
-                      <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Cancelar</button>
-                      <button class="btn btn-success" type="button">Registrar </button>
-                    </div>
+                   @include('modulos.super-usuario.administracion-periodos.period.formulario-periodo') 
+
+                  </div>
+                  <div class="modal-footer mx-0 my-0">
+                    <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Cancelar</button>
+                    <button class="btn btn-success" type="button">Registrar </button>
+                  </div>
                   </form>
                 </div>
               </div>
