0cd1d788b8e8fe3e2f1d47693b2fd60a7ed1c238
GuadalupFranco <franco.ramirez.guadalup@gmail.com>
2022-04-26 14:00:36 -0500

    Actualización en el diseño de card para Panel de Dirección de Carrera

--- resources/views/Modulos/DireccionCarrera/PanelDireccionCarrera/index.blade.php
+++ resources/views/Modulos/DireccionCarrera/PanelDireccionCarrera/index.blade.php
@@ -2,40 +2,38 @@
 
 @section('content')
 
+<h3 class="text-center my-2 mb-4">Panel principal</h3>
+
 <div class="row">
-    <div class="col-sm-4">
-        <div class="card overflow-hidden" style="width: 20rem;">
+    <div class="col-sm-6 col-lg-4 mb-4">
+        <div class="card overflow-hidden" style="max-width: 30rem;">
             <div class="card-img-top"><img class="img-fluid" src="../../assets/img/generic/3.jpg" alt="Card image cap" /></div>
-            <div class="card-body">
-                <h5 class="card-title">Consulta grupos</h5>
-                <!--<p class="card-text"></p>-->
+            <div class="card-img-overlay d-flex align-items-center dropdown">
+                <div class="text-center" style="width: 100%">
+                    <p class="btn btn-falcon-primary me-1 mb-1 dropdown-toggle" id="ConsultaGrupsDropdown" type="button" 
+                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Consulta Grupos</p>
+                    <div class="dropdown-menu dropdown-menu-end py-0" aria-labelledby="ConsultaGrupsDropdown">
+                        <a class="dropdown-item" href="{{ route('ConsultaGruposCI.index') }}">Curso de Inducción</a>
+                        <a class="dropdown-item" href="{{ route('ConsultaGruposBA.index') }}">Bachillerato Avanzado</a>
+                    </div>
+                </div>
             </div>
-            <ul class="list-group list-group-flush mb-3">
-                <li class="list-group-item">
-                    <a class="card-link" href="{{ route('ConsultaGruposCI.index') }}">Curso de Inducción</a>
-                </li>
-                <li class="list-group-item">
-                    <a class="card-link" href="{{ route('ConsultaGruposBA.index') }}">Bachillerato Avanzado</a>
-                </li>
-            </ul>
         </div>
     </div>
 
-    <div class="col-sm-4">
-        <div class="card overflow-hidden" style="width: 20rem;">
+    <div class="col-sm-6 col-lg-4 mb-4">
+        <div class="card overflow-hidden" style="max-width: 30rem;">
             <div class="card-img-top"><img class="img-fluid" src="../../assets/img/generic/7.jpg" alt="Card image cap" /></div>
-            <div class="card-body">
-                <h5 class="card-title">Asigna profesores</h5>
-                <!--<p class="card-text"></p>-->
+            <div class="card-img-overlay d-flex align-items-center dropdown">
+                <div class="text-center" style="width: 100%">
+                    <p class="btn btn-falcon-primary me-1 mb-1 dropdown-toggle" id="AsignaProfesoresDropdown" type="button" 
+                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Asigna Profesores</p>
+                    <div class="dropdown-menu dropdown-menu-end py-0" aria-labelledby="AsignaProfesoresDropdown">
+                        <a class="dropdown-item" href="{{ route('AsignaProfesoresCI.index') }}">Curso de Inducción</a>
+                        <a class="dropdown-item" href="{{ route('AsignaProfesoresBA.index') }}">Bachillerato Avanzado</a>
+                    </div>
+                </div>
             </div>
-            <ul class="list-group list-group-flush mb-3">
-                <li class="list-group-item">
-                    <a class="card-link" href="{{ route('AsignaProfesoresCI.index') }}">Curso de Inducción</a>
-                </li>
-                <li class="list-group-item">
-                    <a class="card-link" href="{{ route('AsignaProfesoresBA.index') }}">Bachillerato Avanzado</a>
-                </li>
-            </ul>
         </div>
     </div>
 </div>
